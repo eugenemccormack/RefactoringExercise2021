3cfd9aa5a90a119532071e9905488c8135069e99
Eugene McCormack <c17745919@mytudublin.ie>
2021-03-07 19:06:17 +0000

    Removed Redundant Variable Assignment and Changed Name of Variable

--- RefactoringExercise2021/src/Employee.java
+++ RefactoringExercise2021/src/Employee.java
@@ -93,14 +93,14 @@ public class Employee{
 
 	// Display Employee details
 	public String toString() {
-		String bool = "";
+		String isFullTime;
 		if (fullTime)
-			bool = "Yes";
+			isFullTime = "Yes";
 		else
-			bool = "No";
+			isFullTime = "No";
 
 		return "Employee ID: " + this.employeeId + "\nPPS Number: " + this.pps + "\nSurname: " + this.surname
 				+ "\nFirst Name: " + this.firstName + "\nGender: " + this.gender + "\nDepartment: " + this.department + "\nSalary: " + this.salary
-				+ "\nFull Time: " + bool;
+				+ "\nFull Time: " + isFullTime;
 	}// end toString
 }// end class Employee
